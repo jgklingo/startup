a05bca13a94857c8e455e9337e2de3472947f774
jgklingo <jgklingo@byu.edu>
2024-11-08 15:53:08 -0700

    add(ability to submit questions)

--- src/questions/questions.jsx
+++ src/questions/questions.jsx
@@ -23,7 +23,20 @@ export function Questions() {
       timePosted: new Date(2024, 10, 8, 15, 0)
     }
   ]
+
+  function submitQuestion(e) {
+    // this function will be replaced with a web service call
+    e.preventDefault();
+    setQuestions([...questions, {
+      userName: 'Test User',
+      text: newQuestion,
+      votes: 5,
+      timePosted: new Date()
+    }]);
+    setNewQuestion('');
+  }
   const [questions, setQuestions] = React.useState(exampleQuestions);  // the example array will be replaced with a web service call
+  const [newQuestion, setNewQuestion] = React.useState('');
   return (
     <main className='container-fluid text-center'>
       <div className="d-flex flex-column align-items-center mt-3">
@@ -32,15 +45,15 @@ export function Questions() {
             <h2>Ask a Question</h2>
             <form>
               <div className="d-flex justify-content-center">
-                <textarea id="question" placeholder="Type your question here..." className="input-group-text mb-3"></textarea>
+                <textarea id="question" placeholder="Type your question here..." value={newQuestion} onChange={(e) => setNewQuestion(e.target.value)} className="input-group-text mb-3"></textarea>
               </div>
-              <p><button type="submit" className="btn btn-primary">Submit</button></p>
+              <p><button type="submit" onClick={(e) => submitQuestion(e)} className="btn btn-primary">Submit</button></p>
             </form>
           </div>
         </div>
 
         {questions.sort((a, b) => b.votes - a.votes).map((question, index) => (
-          <Question userName={question.userName} text={question.text} votes={question.votes} timePosted={question.timePosted} />
+          <Question key={index} userName={question.userName} text={question.text} votes={question.votes} timePosted={question.timePosted} />
         ))}
       </div>
     </main>
