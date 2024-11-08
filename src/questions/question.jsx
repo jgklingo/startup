e6e66d95f58037f08c9c167be8b0b158e834ffff
jgklingo <jgklingo@byu.edu>
2024-11-08 16:14:10 -0700

    enhance(timeSince function)

--- src/questions/question.jsx
+++ src/questions/question.jsx
@@ -5,25 +5,25 @@ export function Question({ userName, text, votes, timePosted }) {
         const seconds = Math.floor((new Date() - date) / 1000);
         let interval = seconds / 31536000;
         if (interval > 1) {
-            return Math.floor(interval) + " years";
+            return Math.floor(interval) + (Math.floor(interval) > 1 ? " years" : " year");
         }
         interval = seconds / 2592000;
         if (interval > 1) {
-            return Math.floor(interval) + " months";
+            return Math.floor(interval) + (Math.floor(interval) > 1 ? " months" : " month");
         }
         interval = seconds / 86400;
         if (interval > 1) {
-            return Math.floor(interval) + " days";
+            return Math.floor(interval) + (Math.floor(interval) > 1 ? "  days" : " day");
         }
         interval = seconds / 3600;
         if (interval > 1) {
-            return Math.floor(interval) + " hours";
+            return Math.floor(interval) + (Math.floor(interval) > 1 ? " hours" : " hour");
         }
         interval = seconds / 60;
         if (interval > 1) {
-            return Math.floor(interval) + " minutes";
+            return Math.floor(interval) + (Math.floor(interval) > 1 ? " minutes" : " minute");
         }
-        return Math.floor(seconds) + " seconds";
+        return Math.floor(seconds) + (Math.floor(interval) > 1 ? " seconds" : " second");
     }
 
     return (
@@ -33,7 +33,7 @@ export function Question({ userName, text, votes, timePosted }) {
             <p className="fst-italic">{timeSince(timePosted)} ago</p>
             <p>{text}</p>
             <p>
-              {votes} votes
+              {votes} {votes === 1 ? 'vote' : 'votes'}
               <a className="ps-2" href="">Upvote</a>
               <a className="ps-2" href="">Downvote</a>
             </p>
